36257f38f5664e3b2cc0e1f14a108105b6383926
Herbert Jones <jones.herbert@gmail.com>
2018-08-18 06:07:03 -0500

    Remove "Security" group

--- groups.lisp
+++ groups.lisp
@@ -3,18 +3,15 @@
 (run-commands "grename Emacs"
               "gnewbg Browser"
               "gnewbg Email"
-              "gnewbg Terminal"
-              "gnewbg Security")
+              "gnewbg Terminal")
 
 (define-frame-preference "Browser"
     (0 t   t :class "Firefox")
-  (1 t   t :class "Google-chrome"))
+  (1 t   t :class "Google-chrome")
+  (2 t t :class "keepassxc"))
 
 (define-frame-preference "Email"
     (0 t   t :class "Thunderbird"))
 
 (define-frame-preference "Terminal"
     (0 t t :title "tmux - main"))
-
-(define-frame-preference "Security"
-    (0 t t :class "keepassxc"))
