8ed3214ea47ac63df1e8962991e73e0897262b70
Herbert Jones <jones.herbert@gmail.com>
2018-08-22 21:00:28 -0500

    Add with-new-window: exec program and run actions on new window

--- clean.lisp
+++ clean.lisp
@@ -3,6 +3,7 @@
 (clear-window-placement-rules)
 
 ;; Clear hooks I use on restart
+(remove-all-hooks *new-window-hook*)
 (remove-all-hooks *focus-group-hook*)
 (remove-all-hooks *focus-window-hook*)
 (remove-all-hooks *focus-frame-hook*)
--- window.lisp
+++ window.lisp
@@ -2,7 +2,8 @@
 (export '(get-x
           get-y
           get-width
-          get-height))
+          get-height
+          with-new-window))
 
 (defgeneric get-x (window)
   (:documentation "Generic get x position"))
@@ -49,3 +50,42 @@
 (defcommand iresize-hfj () ()
   (ratwarp 0 0)
   (run-commands "iresize"))
+
+(defun run-and-act-on-new-window (cmd props timeout function)
+  "Run a command, setup a handler to apply a function to the new window once it's open."
+  (let* (focus-window-handler
+         timeout-handler
+         (timer (run-with-timer timeout nil
+                                #'(lambda ()
+                                    ;; Remove hooks after period of time should something go wrong.
+                                    (my-debug "Timed out!")
+                                    (when focus-window-handler
+                                      (my-debug "Removed focus-window-handler")
+                                      (remove-hook *focus-window-hook* focus-window-handler))
+                                    (when new-window-handler
+                                      (my-debug "Removed new-window-handler")
+                                      (remove-hook *new-window-hook* new-window-handler))))))
+    (setf new-window-handler
+          #'(lambda (new-window)
+              (when (apply 'stumpwm::window-matches-properties-p new-window props)
+                (remove-hook *new-window-hook* new-window-handler)
+                (setf new-window-handler nil)
+                (setf focus-window-handler
+                      #'(lambda (focused-window last-focused-window)
+                          (declare (ignore last-focused-window))
+                          (when (eq new-window focused-window)
+                            (remove-hook *focus-window-hook* focus-window-handler)
+                            (setf focus-window-handler nil)
+                            (cancel-timer timer)
+                            (funcall function new-window))))
+                (add-hook *focus-window-hook* focus-window-handler))))
+    (add-hook *new-window-hook* new-window-handler)
+    (run-shell-command cmd)))
+
+(defmacro with-new-window ((window cmd &key properties (timeout 30))
+                           &body body)
+  "Execute command, on next new window matching properties, run the body.  If no
+properties given, next new window will be acted on."
+  `(run-and-act-on-new-window ,cmd ,properties ,timeout
+                              #'(lambda (,window)
+                                  ,@body)))
