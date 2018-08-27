16c43f146b58a0f77b2fc76796b0faf870f653d4
Herbert Jones <jones.herbert@gmail.com>
2018-08-26 21:38:22 -0500

    Use mouse-follow-policy
    See: https://github.com/stumpwm/stumpwm/pull/495

--- window.lisp
+++ window.lisp
@@ -1,45 +1,5 @@
 (in-package :hfj)
-(export '(get-x
-          get-y
-          get-width
-          get-height
-          with-new-window))
-
-(defgeneric get-x (window)
-  (:documentation "Generic get x position"))
-(defgeneric get-y (window)
-  (:documentation "Generic get y position"))
-(defgeneric get-width (window)
-  (:documentation "Generic get width"))
-(defgeneric get-height (window)
-  (:documentation "Generic get height position"))
-
-(defmethod get-x ((window xlib:window))
-  (xlib:drawable-x window))
-(defmethod get-y ((window xlib:window))
-  (xlib:drawable-y window))
-(defmethod get-width ((window xlib:window))
-  (xlib:drawable-width window))
-(defmethod get-height ((window xlib:window))
-  (xlib:drawable-height window))
-
-(defmethod get-x ((frame stumpwm::frame))
-  (frame-x frame))
-(defmethod get-y ((frame stumpwm::frame))
-  (frame-y frame))
-(defmethod get-width ((frame stumpwm::frame))
-  (frame-width frame))
-(defmethod get-height ((frame stumpwm::frame))
-  (frame-height frame))
-
-(defmethod get-x ((window stumpwm::window))
-  (get-x (window-parent window)))
-(defmethod get-y ((window stumpwm::window))
-  (get-y (window-parent window)))
-(defmethod get-width ((window stumpwm::window))
-  (window-width window))
-(defmethod get-height ((window stumpwm::window))
-  (window-height window))
+(export '(with-new-window))
 
 (defcommand windowlist-all () ()
   (let* ((windows (sort (copy-list (stumpwm::all-windows)) #'string-lessp :key #'stumpwm::window-name))
