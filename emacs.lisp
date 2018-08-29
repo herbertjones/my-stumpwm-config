8cdd954f0c8c39c82ffd2542866b10b709159add
Herbert Jones <jones.herbert@gmail.com>
2018-08-28 20:51:51 -0500

    Determine port with start swank

--- emacs.lisp
+++ emacs.lisp
@@ -3,10 +3,11 @@
 (ql:quickload "swank")
 (swank-loader:init)
 
-(def-menu-command start-swank () () (*default-stumpwm-menu* "Start Swank")
-  (swank:create-server :port 4004
-                       :style swank:*communication-style*
-                       :dont-close t))
+(def-menu-command start-swank (port) ((:number "Port: ")) (*default-stumpwm-menu* "Start Swank")
+  (when port
+    (swank:create-server :port port
+                         :style swank:*communication-style*
+                         :dont-close nil)))
 
 (def-menu-command stop-swank () () (*default-stumpwm-menu* "Stop Swank")
   (swank:stop-server 4004))
