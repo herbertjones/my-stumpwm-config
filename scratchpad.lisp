2b9d7a28af4fe454cd38d2795443bee5e3ac2294
Herbert Jones <jones.herbert@gmail.com>
2018-08-29 21:58:19 -0500

    Cleanup split scratchpad

--- scratchpad.lisp
+++ scratchpad.lisp
@@ -30,7 +30,6 @@
 (remove-hook *destroy-window-hook* #'scratchpad-handle-split-window-destroy)
 (add-hook *destroy-window-hook* #'scratchpad-handle-split-window-destroy)
 
-
 (defun maybe-remove-old-split (moved-from-group moved-from-frame)
   "Remove old frame if empty."
   (let* ((head (stumpwm::frame-head moved-from-group moved-from-frame))
@@ -38,84 +37,6 @@
     (when (and (null (stumpwm::frame-window moved-from-frame))
                (not (atom tree)))
       (remove-split moved-from-group))))
-
-(defun flatten-direction (current-frame direction)
-  "Convert direction list into single direction."
-  (cond ((listp direction)
-         (let* ((w (frame-width current-frame))
-                (h (frame-height current-frame))
-                (allowed (if (< w h)
-                             '(:above :below)
-                             '(:left :right))))
-           (or (first (intersection allowed direction)) :below)))
-        (t
-         direction)))
-
-(defun scratchpad-split-frame (direction ratio group current-frame current-window
-                           scratchpad-window moved-from-group moved-from-frame)
-  "Create a new frame and place the scratchpad in it."
-  (let* ((decided-direction (flatten-direction current-frame direction))
-         (swapped (member decided-direction '(:above :left)))
-         (dir (if (member decided-direction '(:below :above)) :row :column))
-         (r (if swapped ratio (- 1 ratio)))
-         (old-num (stumpwm::frame-number current-frame))
-         (new-num (stumpwm::split-frame group dir r))
-         (target-frame (stumpwm::frame-by-number group (if swapped old-num new-num)))
-         (original-frame (stumpwm::frame-by-number group (if swapped new-num old-num))))
-    (move-window-to-group scratchpad-window group)
-    (maybe-remove-old-split moved-from-group moved-from-frame)
-    (when swapped
-      (stumpwm::migrate-frame-windows group target-frame original-frame))
-    (stumpwm::pull-window scratchpad-window target-frame nil)
-    (when current-window
-      (stumpwm::pull-window current-window original-frame nil))
-    (stumpwm::focus-frame group target-frame)
-    (stumpwm::sync-all-frame-windows group)))
-
-(defun split-toggle (props &key cmd
-                         (ratio *default-split-ratio*)
-                         (direction '(:below :right))
-                         (all-groups *run-or-raise-all-groups*)
-                         (all-screens *run-or-raise-all-screens*))
-  "Display a window in the current group, splitting or focusing.
-
-Direction can be one of: :above :below :left :right
- Or a list with the sides that may be chosen.  The shorter edge decide the split.
-
-Can optionally create the process if needed, however it will initially display
-in the current frame or by whatever rules have been set for new windows.
-"
-  (let* ((group (current-group))
-         (matches (stumpwm::find-matching-windows props all-groups all-screens))
-         (current-frame (stumpwm::tile-group-current-frame group))
-         (current-window (group-current-window group)))
-    (cond ((null matches)
-           (when cmd
-             (run-shell-command cmd)))
-          (t (let* ((scratchpad-window (first matches))
-                    (moved-from-frame (stumpwm::window-frame scratchpad-window))
-                    (moved-from-group (window-group scratchpad-window)))
-               (cond
-                 ;; Currently focused on scratchpad; Hide it
-                 ((and (eq current-frame moved-from-frame)
-                       (eq scratchpad-window (stumpwm::frame-window current-frame)))
-                  (remove-split))
-                 ;; Scratchpad is visible, move to it
-                 ((and (eq moved-from-group group) (window-visible-p scratchpad-window))
-                  (stumpwm::focus-frame moved-from-group moved-from-frame))
-                 ;; Current frame is empty, just display it
-                 ((null (stumpwm::frame-window current-frame))
-                  (move-window-to-group scratchpad-window group)
-                  (maybe-remove-old-split moved-from-group moved-from-frame))
-                 ;; Scratchpad needs a new frame
-                 (t (scratchpad-split-frame direction
-                                            ratio
-                                            group
-                                            current-frame
-                                            current-window
-                                            scratchpad-window
-                                            moved-from-group
-                                            moved-from-frame))))))))
 
 (defun resize-by-gravity (window gravity ratio)
   (let* ((screen (current-screen))
@@ -165,12 +86,109 @@ in the current frame or by whatever rules have been set for new windows.
                                        :width (- new-x-max new-x-min 1)
                                        :height (- new-y-max new-y-min 1))))
 
+(defun flatten-direction (current-frame gravity)
+  "Convert direction list into single direction."
+  (let ((direction '()))
+    (when (member gravity '(:top :top-right :top-left))
+      (push :above direction))
+    (when (member gravity '(:bottom :bottom-right :bottom-left))
+      (push :below direction))
+    (when (member gravity '(:left :top-left :bottom-left))
+      (push :left direction))
+    (when (member gravity '(:right :top-right :bottom-right))
+      (push :right direction))
+    (cond ((and (listp direction)
+                (= 1 (length direction)))
+           (first direction))
+          ((listp direction)
+           (let* ((w (frame-width current-frame))
+                  (h (frame-height current-frame))
+                  (allowed (if (< w h)
+                               '(:above :below)
+                               '(:left :right))))
+             (or (first (intersection allowed direction))
+                 :below)))
+          (t
+           direction))))
+
+(defun scratchpad-split-frame (gravity ratio group current-frame current-window
+                           scratchpad-window moved-from-group moved-from-frame)
+  "Create a new frame and place the scratchpad in it."
+  (let* ((decided-direction (flatten-direction current-frame gravity))
+         (swapped (member decided-direction '(:above :left)))
+         (dir (if (member decided-direction '(:below :above)) :row :column))
+         (r (if swapped ratio (- 1 ratio)))
+         (old-num (stumpwm::frame-number current-frame))
+         (new-num (stumpwm::split-frame group dir r))
+         (target-frame (stumpwm::frame-by-number group (if swapped old-num new-num)))
+         (original-frame (stumpwm::frame-by-number group (if swapped new-num old-num))))
+    (move-window-to-group scratchpad-window group)
+    (maybe-remove-old-split moved-from-group moved-from-frame)
+    (when swapped
+      (stumpwm::migrate-frame-windows group target-frame original-frame))
+    (stumpwm::pull-window scratchpad-window target-frame nil)
+    (when current-window
+      (stumpwm::pull-window current-window original-frame nil))
+    (stumpwm::focus-frame group target-frame)
+    (stumpwm::sync-all-frame-windows group)))
+
+(defun toggle-split-scratchpad (name cmd &key (ratio *default-split-ratio*)
+                                       (gravity :bottom-right)
+                                       (all-groups *run-or-raise-all-groups*)
+                                       (all-screens *run-or-raise-all-screens*))
+  "Create or toggle display of a named scratchpad.  Display by creating a frame."
+  (let ((found (member name *scratch-splits*
+                       :key #'car
+                       :test #'string=)))
+    (cond ((and found
+                (not (typep (cdr (car found))
+                            'stumpwm::tile-window)))
+           ;; Type not correct!
+           nil)
+          (found
+           (let* ((window (cdr (car found)))
+                  (group (current-group))
+                  (current-frame (stumpwm::tile-group-current-frame group))
+                  (current-window (group-current-window group))
+                  (moved-from-frame (stumpwm::window-frame window))
+                  (moved-from-group (window-group window)))
+             (cond
+               ;; Currently focused on scratchpad; Hide it
+               ((and (eq current-frame moved-from-frame)
+                     (eq window (stumpwm::frame-window current-frame)))
+                (remove-split))
+               ;; Scratchpad is visible, move to it
+               ((and (eq moved-from-group group) (window-visible-p window))
+                (stumpwm::focus-frame moved-from-group moved-from-frame))
+               ;; Current frame is empty, just display it
+               ((null (stumpwm::frame-window current-frame))
+                (move-window-to-group window group)
+                (maybe-remove-old-split moved-from-group moved-from-frame))
+               ;; Scratchpad needs a new frame
+               (t (scratchpad-split-frame gravity
+                                          ratio
+                                          group
+                                          current-frame
+                                          current-window
+                                          window
+                                          moved-from-group
+                                          moved-from-frame)))))
+          (t
+           (hfj:with-new-window (window cmd)
+             :init
+             (push (cons name window) *scratch-splits*))))))
+
 (defun toggle-floating-scratchpad (name cmd &key initial-gravity (ratio *default-float-ratio*))
-  "Create a named scratchpad."
+  "Create or toggle display of a named scratchpad.  Display by floating window."
   (let ((found (member name *scratch-floats*
                        :key #'car
                        :test #'string=)))
-    (cond (found
+    (cond ((and found
+                (not (typep (cdr (car found))
+                            'stumpwm::float-window)))
+           ;; Type not correct!
+           nil)
+          (found
            (let ((window (cdr (car found))))
              (cond ((eq (current-window) window)
                     (stumpwm::hide-window window))
