c4567fe34c87dd1f2a6997d4fa8e74711aade678
Herbert Jones <jones.herbert@gmail.com>
2018-08-29 22:50:56 -0500

    Save used swank port for stopping swank

--- emacs.lisp
+++ emacs.lisp
@@ -3,11 +3,17 @@
 (ql:quickload "swank")
 (swank-loader:init)
 
+(defvar *swank-port* nil)
+
 (def-menu-command start-swank (port) ((:number "Port: ")) (*default-stumpwm-menu* "Start Swank")
   (when port
+    (when *swank-port*
+      (ignore-errors (swank:stop-server *swank-port*)))
+    (setf *swank-port* port)
     (swank:create-server :port port
                          :style swank:*communication-style*
                          :dont-close nil)))
 
 (def-menu-command stop-swank () () (*default-stumpwm-menu* "Stop Swank")
-  (swank:stop-server 4004))
+  (when *swank-port*
+    (ignore-errors (swank:stop-server *swank-port*))))
