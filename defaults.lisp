a3579f0f6ac9ce807e837dcea646f0d89bf664ea
Herbert Jones <jones.herbert@gmail.com>
2018-09-01 19:44:47 -0500

    Add defaults.lisp

---
+++ defaults.lisp
@@ -0,0 +1,17 @@
+(in-package :hfj)
+
+(defvar *group-names* '("Emacs"
+                        "Browser"
+                        "Terminal"
+                        "Email")
+  "List of group names to be created.")
+
+(defvar *frame-preferences* '(("Browser"
+                               (0 t t :class "Firefox")
+                               (1 t t :class "Google-chrome")
+                               (2 t t :class "keepassxc"))
+                              ("Email"
+                               (0 t t :class "Thunderbird"))
+                              ("Terminal"
+                               (0 t t :title "tmux - main")))
+  "List of preferences to pass to define-frame-preference.")
--- groups.lisp
+++ groups.lisp
@@ -1,20 +1,10 @@
 (in-package :hfj)
 
-(defvar *group-setup* nil)
-(unless *group-setup*
-  (run-commands "grename Emacs"
-                "gnewbg Browser"
-                "gnewbg Terminal"
-                "gnewbg Email")
-  (setf *group-setup* t))
+(when (consp *group-names*)
+  (grename (first *group-names*))
+  (loop for name in (rest *group-names*)
+        do (add-group (current-screen) name)))
 
-(define-frame-preference "Browser"
-    (0 t   t :class "Firefox")
-  (1 t   t :class "Google-chrome")
-  (2 t t :class "keepassxc"))
-
-(define-frame-preference "Email"
-    (0 t   t :class "Thunderbird"))
-
-(define-frame-preference "Terminal"
-    (0 t t :title "tmux - main"))
+(when (consp *frame-preferences*)
+  (loop for (name . prefs) in *frame-preferences*
+        do `(define-frame-preference name ,@prefs)))
