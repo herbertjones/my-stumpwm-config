b0c6d6f9f7609690b583e1d3e97342046baee160
Herbert Jones <jones.herbert@gmail.com>
2018-09-05 20:57:16 -0500

    Assign main emacs instance to "Emacs" group

--- defaults.lisp
+++ defaults.lisp
@@ -6,7 +6,9 @@
                         "Email")
   "List of group names to be created.")
 
-(defvar *frame-preferences* '(("Browser"
+(defvar *frame-preferences* '(("Emacs"
+                               (0 t t :title "Emacs - main"))
+                              ("Browser"
                                (0 t t :class "Firefox")
                                (1 t t :class "Google-chrome")
                                (2 t t :class "keepassxc"))
